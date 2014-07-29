b055e04bf50d3020f9571c1efc563224294aef73
kojima <kojima@cyber.t.u-tokyo.ac.jp>
2014-07-29 23:48:54 +0900

    dotfileslink path abs->rel

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #! /bin/zsh
-ln -s ~/Documents/Github/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Documents/Github/dotfiles/.zshrc ~/.zshrc
-ln -s ~/Documents/Github/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/Documents/Github/dotfiles/.global_gitignore ~/.global_gitignore
-ln -s ~/Documents/Github/dotfiles/.vim ~/.vim
+ln -s ./.vimrc ~/.vimrc
+ln -s ./.zshrc ~/.zshrc
+ln -s ./.gitconfig ~/.gitconfig
+ln -s ./.global_gitignore ~/.global_gitignore
+ln -s ./.vim ~/.vim
